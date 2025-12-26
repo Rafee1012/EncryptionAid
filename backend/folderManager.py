75f7aabb5be6a9d6c5be2d6b594892f87f844234
rafee01 <rafee01@students.cs.ubc.ca>
2025-12-25 18:12:14 -0800

    Added functionality to accept folder names for terminal commands

--- backend/folderManager.py
+++ backend/folderManager.py
@@ -2,5 +2,10 @@ import subprocess
 
 folders=[]
 
+# runs mkdir in terminal to create folder of name
 def make_folder(name):
-    subprocess.run("mkdir " + name, shell=True)
+    subprocess.run("mkdir " + str(name), shell=True) # ISSUE: MAKING FOLDER WITHIN VS CODE, NOT FINDER
+
+def access_folder(name):
+    subprocess.run("open .")
+    print(name) # OPEN FOLDER WITH NAME FROM FINDER
--- frontend/desktopGUI.py
+++ frontend/desktopGUI.py
@@ -1,22 +1,32 @@
 import dearpygui.dearpygui as dpg
 import sys
 sys.path.append('/Users/rafeeahsan/EncryptionAid/EncryptionAid')
-from backend.folderManager import make_folder, folders
+from backend.folderManager import make_folder, access_folder, folders
 
 screen_dim = {
     "width" : 550,
-    "height" : 300,
+    "height" : 450,
     "x_pos" : 500,
     "y_pos" : 250
 }
 
+# adds new window to create folder
 def create_folder():
-    folder_name = dpg.add_input_text(label="Enter folder name: ", default_value="Enter folder name: ")
-    dpg.add_button(label="Confirm", callback=make_folder(folder_name)) ## ADD BUTTON POSITION
-    folders.append(folder_name)
+    with dpg.window(label="Create Folder",
+                    width=screen_dim["width"]/1.5,
+                    height=screen_dim["height"]/4.5,
+                    pos=[90, 200]):
+        dpg.add_text("Enter folder name:", pos=[20, 30])
+        folder_name = dpg.add_input_text(label="",
+                                        pos=[20, 55])
+        dpg.add_button(label="Confirm", callback=make_folder(folder_name),
+                                        pos=[285, 55])
+        folders.append(folder_name)
 
-
+# opens previously created folder
 def open_folder():
+    with dpg.window():
+        dpg.add_button(label="Confirm", callback=access_folder("NAME")) # ALLOW USE TO CHOOSE SPECIFIC FILE FROM THIS PROGRAM, OPEN IT AND REPLACE 'NAME'
     # access terminal and use open folder_name to open folder
     print()
 
@@ -32,9 +42,9 @@ def main():
     with dpg.window(tag="Primary Window"):
         dpg.add_text("Welcome")
         dpg.add_button(label="Create Folder", pos=[150, screen_dim["y_pos"]/2],
-                       callback=create_folder)
+                        callback=create_folder)
         dpg.add_button(label="Access Folder", pos=[300, screen_dim["y_pos"]/2],
-                       callback=open_folder)
+                        callback=open_folder)
         #dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
 
     ## Main script must setup gui, show viewport, start gui, and destroy context
